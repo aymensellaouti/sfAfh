53f0f64c64a9fc35f3cbfe6a1ccf1cd1e03083c2
aymensellaouti <aymen.sellaouti@gmail.com>
2025-06-19 14:02:14 +0100

    cvController exercice

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -4,13 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CvController extends AbstractController
 {
     #[Route('/cv/{name}/{firstname}/{age}/{section}', name: 'app_cv')]
-    public function index($name, $firstname, $age, $section): Response
+    public function index($name, $firstname, $age, $section, SessionInterface $session): Response
     {
+        if($age < 65) {
+            $this->addFlash('success', 'Rak Mazelt Chabeb :D');
+        }
+
+        //return $this->redirectToRoute('')
         return $this->render('cv/index.html.twig', [
             'name' => $name,
             'firstname' => $firstname,
