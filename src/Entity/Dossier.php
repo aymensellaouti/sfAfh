82ddb4c089d5e5d7793041545c7063c0f5efed07
aymensellaouti <aymen.sellaouti@gmail.com>
2025-06-24 10:01:05 +0100

    creation de la base et de l'entité (table) dossier

---
+++ src/Entity/Dossier.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DossierRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DossierRepository::class)]
+class Dossier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 12)]
+    private ?string $refrence = null;
+
+    #[ORM\Column]
+    private ?\DateTime $creationDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRefrence(): ?string
+    {
+        return $this->refrence;
+    }
+
+    public function setRefrence(string $refrence): static
+    {
+        $this->refrence = $refrence;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTime
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTime $creationDate): static
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+}
