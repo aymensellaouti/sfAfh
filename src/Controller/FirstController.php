7bc5531b5ca4ff42114413c75d52f541e391dac6
aymensellaouti <aymen.sellaouti@gmail.com>
2025-06-30 14:25:45 +0100

    login logou register validate email reset password

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -6,10 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FirstController extends AbstractController
 {
+    #[Route('/email/test')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('aymen.noreply.please@gmail.com')
+            ->to('aymen.sellaouti@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+        return  new Response('<body><html><h1>Email sent</h1></body></html>');
+        // ...
+    }
     #[Route('/hello/{name}', name: 'app_hello')]
     public function hello($name, Request $request): Response
     {
